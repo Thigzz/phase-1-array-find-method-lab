0bf32e6f24d687fc73416d330d726209b17d4f3e
Ike Mwithiga <mwithigaike@gmail.com>
2025-04-08 15:48:10 +0100

    Index.js edit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function superbowlWin(results) {
+    const win = results.find(result => result.result === "W");
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
